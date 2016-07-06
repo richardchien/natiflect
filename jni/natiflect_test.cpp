78a1d8d0e7821bceef880246dcf41c790aa5ae96
Richard Chien <richardchienthebest@gmail.com>
2016-07-06 09:31:44 +0800

    Add Call_Z, Call_B

--- jni/natiflect_test.cpp
+++ jni/natiflect_test.cpp
@@ -37,8 +37,8 @@ void TestClassConstructor(JNIEnv *env) {
     LogTestPass(TAG, passed);
 }
 
-void TestClassCallV(JNIEnv *env) {
-    const string TAG = "TestClassCallV";
+void TestClassCall_V(JNIEnv *env) {
+    const string TAG = "TestClassCall_V";
 
     bool passed = true;
 
@@ -63,11 +63,49 @@ void TestClassCallV(JNIEnv *env) {
     LogTestPass(TAG, passed);
 }
 
+void TestClassCall_Z(JNIEnv *env) {
+    const string TAG = "TestClassCall_Z";
+
+    bool passed = true;
+
+    try {
+        Class clz(env, "java/lang/Character");
+        if (!clz.Call_Z("isDigit", "(I)Z", '0'/*code point 48*/)) {
+            passed = false;
+        }
+    } catch (Exception e) {
+        passed = false;
+    }
+
+    LogTestPass(TAG, passed);
+}
+
+void TestClassCall_B(JNIEnv *env) {
+    const string TAG = "TestClassCall_B";
+
+    bool passed = true;
+
+    try {
+        Class clz(env, "java/lang/Byte");
+        jstring str = env->NewStringUTF("123");
+        jbyte b = clz.Call_B("parseByte", "(Ljava/lang/String;)B", str);
+        if (b != 123) {
+            passed = false;
+        }
+    } catch (Exception e) {
+        passed = false;
+    }
+
+    LogTestPass(TAG, passed);
+}
+
 extern "C" {
 
 JNIEXPORT void JNICALL Java_im_r_1c_java_Main_nativeTestAll(JNIEnv *env, jclass type) {
     TestClassConstructor(env);
-    TestClassCallV(env);
+    TestClassCall_V(env);
+    TestClassCall_Z(env);
+    TestClassCall_B(env);
 }
 
 }
