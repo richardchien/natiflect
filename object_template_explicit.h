29b92543f97ce9ec39289748f06998d2122c3c7b
Richard Chien <richardchienthebest@gmail.com>
2016-07-07 19:32:24 +0800

    Fix unimportant stuffs

--- object_template_explicit.h
+++ object_template_explicit.h
@@ -1,9 +1,29 @@
 //
+// Copyright (c) 2016 Richard Chien
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
 // Created by Richard Chien on 7/7/16.
 //
 
-#ifndef NATIFLECT_TEST_OBJECT_TEMPLATE_EXPLICIT_H
-#define NATIFLECT_TEST_OBJECT_TEMPLATE_EXPLICIT_H
+#ifndef NATIFLECT_OBJECT_TEMPLATE_EXPLICIT_H
+#define NATIFLECT_OBJECT_TEMPLATE_EXPLICIT_H
 
 #include <jni.h>
 
@@ -52,4 +72,4 @@ namespace natiflect {
     class Object<jthrowable>;
 }
 
-#endif //NATIFLECT_TEST_OBJECT_TEMPLATE_EXPLICIT_H
+#endif //NATIFLECT_OBJECT_TEMPLATE_EXPLICIT_H
